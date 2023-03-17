e90acb7b956e039b799275c2e7b85c75c15733d7
Hyeryeon <0168968060@naver.com>
2023-03-17 23:28:33 +0900

    [서혜련] - 요청/응답 life cycle 연습문제3번 수정

--- src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Lesson01Quiz03Controller {
 
 	// http://localhost:8080/lesson01/quiz03/1
-	@RequestMapping("/lesson01/quiz03/1")
+	@RequestMapping("/lesson01/quiz03/1") // 연결 주소 (는 /1)
 	public String quiz03() {
-		return "lesson01/quiz03/1";
+		// 아래 return 에는 application.proper-에 써진 prefix, suffix(.jsp) 제외 경로 쓰기 
+		// 원래는 WEB-INF/jsp/    lesson01/quiz03    .jsp
+		return "lesson01/quiz03";  // jsp view 경로 
 	}
 }
